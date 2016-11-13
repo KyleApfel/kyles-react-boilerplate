4f3851cda9533ffa7a15ba50e0f9099364b1800f
Adam Booth <adam@ambooth.com>
2016-11-13 09:10:06 -0600

    Remove external imports that don't exist in this context

--- src/features/_featureTemplate/actions.js
+++ src/features/_featureTemplate/actions.js
@@ -1,8 +1,4 @@
 import * as types               from './types';
 import * as R                   from 'ramda';
-import { callAPI
-       , handlePayloadResult
-       , handlePayloadResults } from '../../utils/actionHelpers';
-import {}                       from '../../utils/common';
 
 // export const functionName => {}
